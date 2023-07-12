ab0eab8ddadc6cc48f648c641677913e17cc5cb9
stockiNail <stocki.nail@gmail.com>
2023-07-12 08:43:36 +0200

    changed for issue 11398

--- index.ts
+++ index.ts
@@ -1,12 +1,15 @@
-import {Chart, BarController, BarElement, CategoryScale, LinearScale} from 'chart.js';
-import zoomPlugin from 'chartjs-plugin-zoom';
+//import {Chart, BarController, BarElement, CategoryScale, LinearScale, UpdateMode} from 'chart.js';
+import Chart from 'chart.js/auto';
 import annotationPlugin from 'chartjs-plugin-annotation';
-
-Chart.register(BarController, BarElement, CategoryScale, LinearScale);
-Chart.register([ zoomPlugin, annotationPlugin]);
+import zoomPlugin from 'chartjs-plugin-zoom';
+import ChartDataLabels from 'chartjs-plugin-datalabels';
+//Chart.register(BarController, BarElement, CategoryScale, LinearScale);
+Chart.register([ zoomPlugin, annotationPlugin, ChartDataLabels]);
 
 const canvas = document.createElement('canvas');
 const ctx = canvas.getContext('2d');
+
+//const a:UpdateMode = 'zoom';
 
 export const chart = new Chart(ctx, {
   type: 'bar',
@@ -17,9 +20,14 @@ export const chart = new Chart(ctx, {
     }]
   },
   options: {
-    plugins: {
+    plugins: false
+/*    plugins: {
+	  tooltip: {},
+	  legend: false,
       annotation: { },
-      zoom: { }
+      zoom: { },
+	  datalabels: {}
     }
+*/    
   }
 });
